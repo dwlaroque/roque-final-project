150daa7b67b9dbb1d80b94d10b890fe5548c58ac
dwlaroque <dwlaroque@gmail.com>
2026-01-21 12:09:03 -0500

    after task 4

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+def get_dominant_emotion(emotions):
+    if not emotions:
+        raise ValueError("Emotions dictionary is empty.")
+    return max(emotions, key = emotions.get)
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    obj = {"raw_document": { "text": text_to_analyze }}
+    response = requests.post(url, json = obj, headers = headers)
+    formatted_response = json.loads(response.text)
+    prediction = formatted_response['emotionPredictions']
+    
+    # Access Main Emotion Score
+    emotions = prediction[0]['emotion']
+
+    # Access Individual Emotion Score
+    anger_score = prediction[0]['emotion']['anger']
+    disgust_score = prediction[0]['emotion']['disgust']
+    fear_score = prediction[0]['emotion']['fear']
+    joy_score = prediction[0]['emotion']['joy']
+    sadness_score = prediction[0]['emotion']['sadness']
+    
+    #Modified Output Format
+    emotions = {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score
+    }
+    
+    dominant_emotion = max(emotions, key = emotions.get)
+    emotions["dominant_emotion"] = dominant_emotion
+    return emotions
